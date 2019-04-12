797dc0b9eeb1200aa927ec39e4013e5adb0b9319
Andrea Nasuto <andrea.nasuto@gmail.com>
2019-04-12 22:51:45 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -83,16 +83,15 @@ class MusicLibraryController
   end
 
   def play_song
-    puts "Which song number would you like to play?"
-    input = ""
     self.list_songs
+    input = ""
     loop do
       puts "Which song number would you like to play?"
       input = gets.strip
       songs_sorted = Song.all.sort { |e,f| e.name <=> f.name}
       if input.to_i <= songs_sorted.uniq.size
-        break
         puts "Playing #{songs_sorted[input.to_i - 1].name} by #{songs_sorted[input.to_i - 1].artist.name}"
+        break
       end
     end
   end
